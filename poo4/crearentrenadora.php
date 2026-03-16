a1885acdb09dc4446e1ad1c7dcca5ca7f1b67629
garosv-tech <garosv@campusdigitalfp.com>
2026-03-16 14:06:17 +0100

    Update crearentrenadora.php

--- poo4/crearentrenadora.php
+++ poo4/crearentrenadora.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'entrenadora.php';
+require_once 'Entrenadora.php';
 session_start();
 ?>
 
@@ -9,20 +9,16 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crear Entrenadora</title>
-    <link rel="icon" type="image/x-icon" href="img/pokeball.png">
     <link rel="stylesheet" href="estilos.css">
 </head>
-<body class='fondo-tercero'>
+<body>
     <section>
         <h1>Crear Entrenadora</h1>
         <form method="POST" action="">
-        <input type="radio" id="deldata" name="opcion1" value="deldata">
-        <label for="deldata">Borrar datos</label>
-        <p></p>
         <label for="nombre">Nombre</label>
         <input type="text" name="nombre"><p></p>
 
-        <input type="submit" value="Crear Entrenadora">
+        <input type="submit" value="crear">
         </form>
     </section>
 
@@ -35,16 +31,10 @@ session_start();
     if(isset($_POST["nombre"])){
         $nombre = $_POST["nombre"];
 
-        if (isset($_POST['opcion1'])&&$_POST['opcion1']==='deldata'){ 
-        destruirDatos(); 
-        $_SESSION['entrenadoras']=[];
-        $mensaje="Datos borrados.";
-        } else {
         $entrenadora = new Entrenadora($nombre);
         $_SESSION["entrenadoras"][]=$entrenadora;
 
-        $mensaje="Entrenadora creada con éxito.";
-    }
+        $mensaje= "Entrenadora creada con éxito.";
     }
 
     if (isset($mensaje)){
@@ -57,24 +47,20 @@ session_start();
      <section>
         <h2>Entrenadoras creadas</h2>
         <?php
-        if (count($_SESSION["entrenadoras"]) > 0) {
+if(!empty($_POST['nombre'])){
+        if (count($_SESSION["entrenadoras"]) > 0 ) {
             foreach ($_SESSION["entrenadoras"] as $entrenadora) {
                 echo "<p>" . $entrenadora->mostrarInfo() . "</p>";
-                echo "<p> ----------------------- </p>";
             }
         } else {
             echo "<p>No hay entrenadoras creadas todavía</p>";
         }
-
-
-        function destruirDatos(){ 
-            $_SESSION['entrenadoras']=[];  
         }
         ?>
     </section>
     <section>
-        <a class="button" href="crearpokemon.php">Crear Pokemon</a>
-        <a class="button" href="index.php">Ir a gestion</a>
+        <a href="crearpokemon.php">Crear Pokemon</a>
+        <a href="gestion.php">Ir a gestion</a>
     </section>
 </body>
 </html>
